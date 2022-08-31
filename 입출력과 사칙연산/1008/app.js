d999c243456613e83c24f8b1ef1136d24e87d9b5
wang-yurin <king_1994@naver.com>
2022-08-31 17:46:38 +0900

    Solve : 1008 문제 해결

---
+++ 입출력과 사칙연산/1008/app.js
@@ -0,0 +1,20 @@
+/*
+A/B
+문제
+두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a / b);
