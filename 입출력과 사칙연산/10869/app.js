187a9da1e7abcc06cd2991cf76c5ef7ddcf94acb
wang-yurin <king_1994@naver.com>
2022-09-01 11:34:24 +0900

    Solve : 10869 문제 해결

---
+++ 입출력과 사칙연산/10869/app.js
@@ -0,0 +1,25 @@
+/*
+사칙연산
+
+문제
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+출력
+첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
