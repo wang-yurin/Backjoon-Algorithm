d308aeb79760d8bc737cd07f559646e31496c6c5
wang-yurin <king_1994@naver.com>
2022-10-03 00:17:54 +0900

    Solve : 2292 문제 해결

--- 기본수학1/2292/app.js
+++ 기본수학1/2292/app.js
@@ -2,4 +2,14 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim();
 
+var count = 1; // 1부터 출발하니까
+var sum = 1;
+while (sum < input) {
+  sum += 6 * count;
+  count++;
+}
+console.log(count);
+
 // 규칙 찾기?
+// 일단 1을 기준으로 어떤 규칙이 있는가 살펴보았음
+// 6의 배수로 커지는 듯
