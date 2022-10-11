56c1b4b7507a9963a8ae9b166677a8bfe3a8df47
wang-yurin <king_1994@naver.com>
2022-10-11 14:16:03 +0900

    Solve : 10814 문제 해결

---
+++ 정렬/10814/app.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+input.shift();
+let result = input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+console.log(result.join("\n"));
