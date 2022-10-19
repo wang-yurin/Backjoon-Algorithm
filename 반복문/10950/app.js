a6ff6e556ac6dd5a055a4ca0dd5a2856f077e961
wang-yurin <king_1994@naver.com>
2022-10-19 13:08:21 +0900

    Solve : 10950 문제 해결

--- 반복문/10950/app.js
+++ 반복문/10950/app.js
@@ -16,12 +16,17 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().split("\n");
 
-for (i = 1; i <= input[0]; i++) {
-  let arr = input[i].split(" ");
-  console.log(parseInt(arr[0]) + parseInt(arr[1]));
-}
+// for (i = 1; i <= input[0]; i++) {
+//   let arr = input[i].split(" ");
+//   console.log(parseInt(arr[0]) + parseInt(arr[1]));
+// }
 
 // input 배열 인덱스0에는 테스트 케이스의 숫자가 들어가기 때문에
 // 그 다음 인덱스부터(그렇기 때문에 for문 조건을 i=1) 순회를 하면서 공백을 제거하고 배열을 만들어준다.
 // 생성된 배열 안 요소들이 문자열이기 때문에 숫자형으로 형변환 한다.
 // 생성된 배열로 값을 더해준다.
+
+input.shift();
+console.log(
+  input.map((v) => v.split(" ")).map((v) => v.reduce((acc, cur) => +acc + +cur))
+);
