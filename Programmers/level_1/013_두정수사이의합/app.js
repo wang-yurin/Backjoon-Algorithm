952f40dc8edcf8a2f0846e1b255ad0cc726c96c2
wang-yurin <king_1994@naver.com>
2022-11-26 15:21:59 +0900

    Solve : 두 정수 사이의 합 문제 해결 프로그래머스 레벨 1

---
+++ Programmers/level_1/013_두정수사이의합/app.js
@@ -0,0 +1,17 @@
+function solution(a, b) {
+  if (a === b) return a;
+
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
+
+  if (a > b) {
+    for (let i = a; i >= b; i--) {
+      sum += i;
+    }
+  }
+  return sum;
+}
