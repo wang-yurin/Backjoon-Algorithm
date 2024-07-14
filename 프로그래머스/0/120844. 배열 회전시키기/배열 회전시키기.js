f6835a5cc27ba8e9ac0417cc1da194f67ed48695
Yurin Wang <101461874+wang-yurin@users.noreply.github.com>
2024-07-14 21:44:50 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,9 @@
+function solution(numbers, direction) {
+    if (direction === 'right') {
+        numbers.unshift(numbers.pop())
+        return numbers
+    } else {
+        numbers.push(numbers.shift())
+        return numbers
+    }
+}
