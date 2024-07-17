b1fb55343bea9771f9a91cb89131478153073c8e
Yurin Wang <101461874+wang-yurin@users.noreply.github.com>
2024-07-17 15:08:21 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,5 @@
+function solution(a, b, c) {
+    if (a !== b && b !== c && a !== c) return a + b + c
+    else if (a === b && b === c) return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    else return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2))
+}
