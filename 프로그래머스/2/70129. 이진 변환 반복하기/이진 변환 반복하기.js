10afb51ef1f32d9c954831bcbe9b1f0654604a5f
Yurin Wang <101461874+wang-yurin@users.noreply.github.com>
2024-08-10 19:44:13 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.97 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,12 +1,3 @@
 function solution(s) {
-    let count = 0;
-    let length = 0;
-    
-    while (s !== '1') {
-        let len = s.split('0').join('').length;
-        length += s.length - len;
-        s = len.toString(2);
-        count++
-    }
-    return [count, length]
+    return s.split('0').length
 }
